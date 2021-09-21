490fdc27c7b2db2dd424e08e759f7c4785dceac2
Mindaugas Jačionis <m.jacionis@gmail.com>
2021-09-21 19:19:16 +0300

    Refactored to single event listener for actions

--- index.js
+++ index.js
@@ -1,39 +1,4 @@
-const output = document.querySelector("#output");
-
-document.querySelector("#triangle").addEventListener("click", () => {
-  let shape = "";
-
-  for (let floor = 1; floor <= 6; floor++) {
-    for (let flat = 0; flat < floor; flat++) {
-      shape = shape + "*";
-    }
-
-    shape = shape + "<br />";
-  }
-
-  output.innerHTML = shape;
-});
-
-// document.querySelector("#pyramid").addEventListener("click", () => {
-//   const maxFloors = 7;
-//   let shape = "";
-
-//   for (let floor = 1; floor <= maxFloors; floor++) {
-//     for (let spaces = 0; spaces < maxFloors - floor; spaces++) {
-//       shape = shape + "&nbsp;";
-//     }
-
-//     for (let flat = 0; flat < floor; flat++) {
-//       shape = shape + "*&nbsp;";
-//     }
-
-//     shape = shape + "<br />";
-//   }
-
-//   output.innerHTML = shape;
-// });
-
-document.querySelector("#pyramid").addEventListener("click", () => {
+function getPyramid() {
   const maxFloors = 7;
   let shape = "";
 
@@ -49,10 +14,10 @@ document.querySelector("#pyramid").addEventListener("click", () => {
     shape = shape + "<br />";
   }
 
-  output.innerHTML = shape;
-});
+  return shape;
+}
 
-document.querySelector("#reverse-pyramid").addEventListener("click", () => {
+function getReversePyramid() {
   const maxFloors = 7;
   let shape = "";
 
@@ -68,9 +33,42 @@ document.querySelector("#reverse-pyramid").addEventListener("click", () => {
     shape = shape + "<br />";
   }
 
-  output.innerHTML = shape;
-});
+  return shape;
+}
 
-document.querySelector("#clear").addEventListener("click", () => {
-  output.innerHTML = null;
+function getTriangle() {
+  let shape = "";
+
+  for (let floor = 1; floor <= 6; floor++) {
+    for (let flat = 0; flat < floor; flat++) {
+      shape = shape + "*";
+    }
+
+    shape = shape + "<br />";
+  }
+
+  return shape;
+}
+
+function getShape(name) {
+  switch (name) {
+    case "clear":
+      return null;
+    case "triangle":
+      return getTriangle();
+    case "pyramid":
+      return getPyramid();
+    case "reverse-pyramid":
+      return getReversePyramid();
+    default:
+      return null;
+  }
+}
+
+document.querySelector("#actions").addEventListener("click", (event) => {
+  if (event.target.tagName === "BUTTON") {
+    const shape = getShape(event.target.id);
+
+    document.querySelector("#output").innerHTML = shape;
+  }
 });
